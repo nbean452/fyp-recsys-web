f4e6647bcb109e287731438cccd4b682e5034a1f
Nicholas Benedict <19086235d@connect.polyu.hk>
2022-12-22 13:34:03 +0700

    Update reducers.ts

--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -1,7 +1,9 @@
 import searchSliceReducer from "./slices/searchSlice";
+import userAgreementReducer from "./slices/userAgreementSlice";
 
 const reducers = {
   courseSearch: searchSliceReducer,
+  userAgreement: userAgreementReducer,
 };
 
 export default reducers;
