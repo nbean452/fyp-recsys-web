201ae09e484185aaeb43e376e0fbabac201884f0
Nicholas Benedict <19086235d@connect.polyu.hk>
2022-12-28 12:50:57 +0700

    change static folder

--- src/pages/docs.tsx
+++ src/pages/docs.tsx
@@ -5,9 +5,9 @@ import Layout from "@components/Layout";
 
 const DocsPage: NextPage = () => (
   <Layout>
-    <Link href="/static/project_outline.pdf">Project Outline</Link>
-    {/* <Link href="/static/project_outline.pdf">Interim Report</Link>
-    <Link href="/static/project_outline.pdf">Final Report</Link> */}
+    <Link href="/docs/project_outline.pdf">Project Outline</Link>
+    {/* <Link href="/docs/project_outline.pdf">Interim Report</Link>
+    <Link href="/docs/project_outline.pdf">Final Report</Link> */}
   </Layout>
 );
 
