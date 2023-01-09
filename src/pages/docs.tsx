221e9f9f226d781c1252e4b3596b10a151435fd8
Nicholas Benedict <19086235d@connect.polyu.hk>
2023-01-09 22:30:09 +0700

    add interim report

--- src/pages/docs.tsx
+++ src/pages/docs.tsx
@@ -6,8 +6,8 @@ import Layout from "@components/Layout";
 const DocsPage: NextPage = () => (
   <Layout>
     <Link href="/docs/project_outline.pdf">Project Outline</Link>
-    {/* <Link href="/docs/project_outline.pdf">Interim Report</Link>
-    <Link href="/docs/project_outline.pdf">Final Report</Link> */}
+    <Link href="/docs/interim_report.pdf">Interim Report</Link>
+    {/* <Link href="/docs/project_outline.pdf">Final Report</Link> */}
   </Layout>
 );
 
