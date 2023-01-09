473065c652edd1d32a9c056f84552a6cda87289e
Nicholas Benedict <19086235d@connect.polyu.hk>
2023-01-09 22:26:44 +0700

    move course list to /courses

--- src/pages/courses.tsx
+++ src/pages/courses.tsx
@@ -1,14 +1,64 @@
-import { Typography } from "antd";
+import { useEffect, useState } from "react";
+
+import { Typography, Row, Col, Card } from "antd";
 import { NextPage } from "next";
+import superagent from "superagent";
 
 import Layout from "@components/Layout";
+import SearchCourse from "@components/SearchCourse";
+import { StyledLink } from "@components/StyledComponents";
+import type { CourseWithRating } from "@constants/types";
 
 const CoursesPage: NextPage = () => {
   const { Text } = Typography;
+  const { Meta } = Card;
 
+  const [courses, setCourses] = useState<Array<CourseWithRating>>([]);
+
+  // superagent
+  //   .post("/api/pet")
+  //   .send({ name: "Manny", species: "cat" }) // sends a JSON post body
+  //   .set("X-API-Key", "foobar")
+  //   .set("accept", "json")
+  //   .end((err, res) => {
+  //     // Calling the end function will send the request
+  //   });
+
+  useEffect(() => {
+    // fetch("http://localhost:9000/courses")
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    //   .then((json: Array<Course>) => {
+    //     setCourses(json);
+    //   });
+    superagent.get("http://localhost:9000/courses").end((err, res) => {
+      console.log("res.body:", res.body.results);
+      setCourses(res.body.results);
+    });
+  }, []);
   return (
     <Layout>
       <Text>Courses</Text>
+
+      <SearchCourse />
+
+      <Row gutter={[16, 16]}>
+        {courses.map((item, index) => (
+          <Col key={index} lg={8} md={12} sm={24} xl={6} xs={24}>
+            <Card>
+              <Meta
+                description={<Text ellipsis>{item.name}</Text>}
+                title={
+                  <StyledLink href={`/course/${item.code}`}>
+                    {item.code}
+                  </StyledLink>
+                }
+              />
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </Layout>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,100 +1,18 @@
-import { Card, Col, Row, Typography } from "antd";
-import kebabCase from "lodash/kebabCase";
+import { Typography } from "antd";
 import { NextPage } from "next";
 import useTranslation from "next-translate/useTranslation";
 
-import Image from "@components/Image";
 import Layout from "@components/Layout";
-import SearchCourse from "@components/SearchCourse";
-import { StyledLink } from "@components/StyledComponents";
 
 const HomePage: NextPage = () => {
   const { t } = useTranslation("home");
 
-  const { Meta } = Card;
   const { Text, Title } = Typography;
-
-  // placeholder
-  const courses = [
-    {
-      course: {
-        code: "CS 115",
-        name: "Introduction to Computer Science 1",
-      },
-      imageURL: "https://source.unsplash.com/random/640x360/?city,night",
-    },
-    {
-      course: {
-        code: "MATH 135",
-        name: "Algebra for Honours Mathematics",
-      },
-      imageURL: "https://source.unsplash.com/random/640x360/?city",
-    },
-    {
-      course: {
-        code: "MATH 137",
-        name: "Calculus 1 for Honours Mathematics",
-      },
-      imageURL: "https://source.unsplash.com/random/640x360/?night",
-    },
-    {
-      course: {
-        code: "CS 135",
-        name: "Designing Functional Programs",
-      },
-      imageURL: "https://source.unsplash.com/random/640x360/?forest",
-    },
-    {
-      course: {
-        code: "STAT 230",
-        name: "Probability",
-      },
-      imageURL: "https://source.unsplash.com/random/640x360/?river",
-    },
-    {
-      course: {
-        code: "CS 240",
-        name: "Data Structures and Data Management",
-      },
-      imageURL: "https://source.unsplash.com/random/640x360/?canyon",
-    },
-  ];
 
   return (
     <Layout>
       <Title>{t`heading`}</Title>
       <Text>{t`subheading`}</Text>
-
-      <SearchCourse />
-
-      <Row gutter={[16, 16]}>
-        {courses.map((item, index) => (
-          <Col key={index} lg={8} md={12} sm={24} xl={6} xs={24}>
-            <Card
-              cover={
-                <StyledLink href={`/course/${kebabCase(item.course.code)}`}>
-                  <Image
-                    alt={item.course.name}
-                    height={360}
-                    src={item.imageURL}
-                    width={640}
-                    priority
-                  />
-                </StyledLink>
-              }
-            >
-              <Meta
-                description={<Text ellipsis>{item.course.name}</Text>}
-                title={
-                  <StyledLink href={`/course/${kebabCase(item.course.code)}`}>
-                    {item.course.code}
-                  </StyledLink>
-                }
-              />
-            </Card>
-          </Col>
-        ))}
-      </Row>
     </Layout>
   );
 };
