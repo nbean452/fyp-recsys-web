abaa94eff99185dc1cd2bd3a83c0abe39d7e7533
Nicholas Benedict <19086235d@connect.polyu.hk>
2023-01-18 23:50:51 +0700

    add fallback to static path

--- src/pages/course/[code].tsx
+++ src/pages/course/[code].tsx
@@ -36,40 +36,21 @@ const CourseSlugPage: NextPage<CourseSlugProps> = ({ code }) => {
   );
 };
 
-// TODO: improve this! maybe make hook for this
-// export const getServerSideProps: GetServerSideProps = async ({ query }) => {
-//   // const { data: course, isError, isFetching } = useGetCourseQuery(query.code);
-//   console.log("query:", query);
-//   return {
-//     props: { code: query.code },
-//   };
-// };
-
 export const getStaticPaths: GetStaticPaths = async () => {
   const url =
     process.env.NODE_ENV === "production"
       ? (process.env.NEXT_PUBLIC_PROD_URL as string)
       : (process.env.NEXT_PUBLIC_DEV_URL as string);
 
-  const res = await fetch(`${url}/courses/?limit=1000&page=0`);
+  const res = await fetch(`${url}/courses/`);
   const data = await res.json();
 
-  // Get the paths we want to pre-render based on posts
-  // const paths = posts.map((post) => ({
-  //   params: { id: post.id },
-  // }));
-  // console.log("data.results:", data.results);
-
   const courses: CourseWithRating[] = data.results;
-  // const json = courses;
-
-  // console.log("json:", json);
-  // console.log("courses:", courses);
 
   const paths = courses.map((course) => ({ params: { code: course.code } }));
 
   return {
-    fallback: false, // can also be true or 'blocking'
+    fallback: true, // can also be true or 'blocking'
     paths,
   };
 };
@@ -77,7 +58,6 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { code } = params as { code: string };
   return {
-    // Passed to the page component as props
     props: { code },
   };
 };
