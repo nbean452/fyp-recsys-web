024d6b35c7f18ee6e8cea6b4a7020d1e2b22c894
Nicholas Benedict <nicholasb1537@gmail.com>
2023-02-11 16:15:23 +0800

    remove next-progress

--- src/constants/color.ts
+++ src/constants/color.ts
@@ -1,6 +1,5 @@
 const color = {
   header: "white",
-  progress: "#1677ff",
 };
 
 export default color;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,11 @@
 import { useEffect, useState } from "react";
 
-import NextProgress from "next-progress";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
 import Disclaimer from "@components/Disclaimer";
 import GlobalStyle from "@components/GlobalStyle";
-import color from "@constants/color";
 import configureStore from "@features/configureStore";
 
 const App = ({ Component, pageProps }: AppProps) => {
@@ -22,7 +20,6 @@ const App = ({ Component, pageProps }: AppProps) => {
   return typeof window === "undefined" || !showChild ? null : (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <NextProgress color={color.progress} options={{ showSpinner: false }} />
         <GlobalStyle />
         <Component {...pageProps} />
         <Disclaimer />
