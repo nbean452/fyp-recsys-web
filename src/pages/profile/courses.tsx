71f77e41e4a3948c104e67959fbbbd4bf0c5cc05
Nicholas Benedict <nicholasb1537@gmail.com>
2023-02-11 16:15:34 +0800

    auth check

--- src/pages/profile/courses.tsx
+++ src/pages/profile/courses.tsx
@@ -1,13 +1,20 @@
 import { Typography } from "antd";
+import { useRouter } from "next/router";
 
 import Layout from "@components/Layout";
+import { useSelector } from "@utils/hooks";
 
 const ProfileCoursesPage = () => {
   const { Title, Text } = Typography;
 
+  const { username } = useSelector((state) => state.auth);
+
+  const router = useRouter();
+
+  if (!username) router.push("/");
   return (
     <Layout>
-      <Title>Title</Title>
+      <Title>Profile Courses</Title>
       <Text>Content</Text>
     </Layout>
   );
