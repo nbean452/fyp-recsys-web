fd1fd0d856ef8e8220a29d3f66895e89f625a7da
Nicholas Benedict <nicholasb1537@gmail.com>
2023-03-08 12:25:15 +0800

    update getImg.ts

--- src/utils/getImg/getImg.ts
+++ src/utils/getImg/getImg.ts
@@ -1,7 +1,9 @@
 /* eslint-disable global-require */
 
 const paths = {
+  confused: require("public/img/confused.jpg"),
   // wallpaper: require("public/svg/wallpaper.svg"),
+  placeholder: require("public/img/placeholder.jpg"),
 };
 
 type ImgPaths = keyof typeof paths;
