ad1b88f3c7efd9d1fd50f4151057eae1bb78af0a
Nicholas Benedict <nicholasb1537@gmail.com>
2024-01-23 16:28:23 +0800

    updated docs.tsx

--- src/pages/docs.tsx
+++ src/pages/docs.tsx
@@ -1,14 +1,13 @@
-import { NextPage } from "next";
+import type { NextPage } from "next";
 import Link from "next/link";
 
 import Layout from "@components/Layout";
 
 const DocsPage: NextPage = () => (
   <Layout>
-    <Link href="/docs/project_outline.pdf">Project Outline</Link>
-    <Link href="/docs/interim_report.pdf">Interim Report</Link>
-    {/* //TODO: Add final report when available */}
-    {/* <Link href="/docs/final_report.pdf">Final Report</Link> */}
+    <Link href="/docs/project-outline.pdf">Project Outline</Link>
+    <Link href="/docs/interim-report.pdf">Interim Report</Link>
+    <Link href="/docs/final-report.pdf">Final Report</Link>
   </Layout>
 );
 
